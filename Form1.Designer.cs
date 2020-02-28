f5f3a8049928eeb3d8b8e4a56dbf3b45b0f21719
JoniDed01 <61559033+JoniDed01@users.noreply.github.com>
2020-02-28 15:31:45 +0300

    exemple

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Caesar_s_cypher
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemOptions = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemBackgroundColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemButtonStyle = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelAction = new System.Windows.Forms.Label();
+            this.comboBoxAction = new System.Windows.Forms.ComboBox();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.numericUpDownKey = new System.Windows.Forms.NumericUpDown();
+            this.labelText = new System.Windows.Forms.Label();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonDo = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKey)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
+            this.toolStripMenuItemOptions});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(628, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemOpen,
+            this.toolStripMenuItemSave,
+            this.toolStripMenuItemExit});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItemFile.Text = "Файл";
+            // 
+            // toolStripMenuItemOptions
+            // 
+            this.toolStripMenuItemOptions.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemBackgroundColor,
+            this.toolStripMenuItemButtonStyle});
+            this.toolStripMenuItemOptions.Name = "toolStripMenuItemOptions";
+            this.toolStripMenuItemOptions.Size = new System.Drawing.Size(83, 20);
+            this.toolStripMenuItemOptions.Text = "Параметры";
+            // 
+            // toolStripMenuItemOpen
+            // 
+            this.toolStripMenuItemOpen.Name = "toolStripMenuItemOpen";
+            this.toolStripMenuItemOpen.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemOpen.Text = "Открыть";
+            this.toolStripMenuItemOpen.Click += new System.EventHandler(this.toolStripMenuItemOpen_Click);
+            // 
+            // toolStripMenuItemSave
+            // 
+            this.toolStripMenuItemSave.Name = "toolStripMenuItemSave";
+            this.toolStripMenuItemSave.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemSave.Text = "Сохранить";
+            this.toolStripMenuItemSave.Click += new System.EventHandler(this.toolStripMenuItemSave_Click);
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemExit.Text = "Выход";
+            this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
+            // 
+            // toolStripMenuItemBackgroundColor
+            // 
+            this.toolStripMenuItemBackgroundColor.Name = "toolStripMenuItemBackgroundColor";
+            this.toolStripMenuItemBackgroundColor.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemBackgroundColor.Text = "Цвет фона";
+            this.toolStripMenuItemBackgroundColor.Click += new System.EventHandler(this.toolStripMenuItemBackgroundColor_Click);
+            // 
+            // toolStripMenuItemButtonStyle
+            // 
+            this.toolStripMenuItemButtonStyle.Name = "toolStripMenuItemButtonStyle";
+            this.toolStripMenuItemButtonStyle.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemButtonStyle.Text = "Стиль кнопок";
+            this.toolStripMenuItemButtonStyle.Click += new System.EventHandler(this.toolStripMenuItemButtonStyle_Click);
+            // 
+            // labelAction
+            // 
+            this.labelAction.AutoSize = true;
+            this.labelAction.Location = new System.Drawing.Point(12, 24);
+            this.labelAction.Name = "labelAction";
+            this.labelAction.Size = new System.Drawing.Size(60, 13);
+            this.labelAction.TabIndex = 1;
+            this.labelAction.Text = "Действие:";
+            // 
+            // comboBoxAction
+            // 
+            this.comboBoxAction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAction.FormattingEnabled = true;
+            this.comboBoxAction.Items.AddRange(new object[] {
+            "Расшифровать",
+            "Зашифровать"});
+            this.comboBoxAction.Location = new System.Drawing.Point(13, 41);
+            this.comboBoxAction.Name = "comboBoxAction";
+            this.comboBoxAction.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxAction.TabIndex = 2;
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Location = new System.Drawing.Point(13, 69);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(36, 13);
+            this.labelKey.TabIndex = 3;
+            this.labelKey.Text = "Ключ:";
+            // 
+            // numericUpDownKey
+            // 
+            this.numericUpDownKey.Location = new System.Drawing.Point(13, 85);
+            this.numericUpDownKey.Name = "numericUpDownKey";
+            this.numericUpDownKey.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownKey.TabIndex = 4;
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(147, 24);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(40, 13);
+            this.labelText.TabIndex = 5;
+            this.labelText.Text = "Текст:";
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(151, 41);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(460, 105);
+            this.richTextBox.TabIndex = 6;
+            this.richTextBox.Text = "";
+            // 
+            // buttonDo
+            // 
+            this.buttonDo.Location = new System.Drawing.Point(13, 112);
+            this.buttonDo.Name = "buttonDo";
+            this.buttonDo.Size = new System.Drawing.Size(75, 23);
+            this.buttonDo.TabIndex = 7;
+            this.buttonDo.Text = "Выполнить";
+            this.buttonDo.UseVisualStyleBackColor = true;
+            this.buttonDo.Click += new System.EventHandler(this.buttonDo_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 159);
+            this.Controls.Add(this.buttonDo);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.numericUpDownKey);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.comboBoxAction);
+            this.Controls.Add(this.labelAction);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form";
+            this.Text = "Шифр цезаря";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKey)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOptions;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpen;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemSave;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemBackgroundColor;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemButtonStyle;
+        private System.Windows.Forms.Label labelAction;
+        private System.Windows.Forms.ComboBox comboBoxAction;
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.NumericUpDown numericUpDownKey;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonDo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Caesar_s_cypher
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        const string ALPHABET_LOWER = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        const string ALPHABET_UPPER = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+
+        public Form()
+        {
+            InitializeComponent();
+            numericUpDownKey.Maximum = ALPHABET_LOWER.Length - 1;
+            comboBoxAction.SelectedIndex = 0;
+        }
+
+        private void buttonDo_Click(object sender, EventArgs e)
+        {
+            CaesarCipher();
+        }
+
+        static char NewCharCipher(char oldChar, int shift)
+        {
+            if (ALPHABET_LOWER.Contains(oldChar.ToString()))
+                return ALPHABET_LOWER[(shift + ALPHABET_LOWER.IndexOf(oldChar)) % ALPHABET_LOWER.Length];
+            if (ALPHABET_UPPER.Contains(oldChar.ToString()))
+                return ALPHABET_UPPER[(shift + ALPHABET_UPPER.IndexOf(oldChar)) % ALPHABET_UPPER.Length];
+            return oldChar;
+        }
+
+        static char NewCharDecrypt(char oldChar, int shift)
+        {
+            if (ALPHABET_LOWER.Contains(oldChar.ToString()))
+                return ALPHABET_LOWER[(ALPHABET_LOWER.Length - shift + ALPHABET_LOWER.IndexOf(oldChar)) % ALPHABET_LOWER.Length];
+            if (ALPHABET_UPPER.Contains(oldChar.ToString()))
+                return ALPHABET_UPPER[(ALPHABET_UPPER.Length - shift + ALPHABET_UPPER.IndexOf(oldChar)) % ALPHABET_UPPER.Length];
+            return oldChar;
+        }
+
+        void CaesarCipher()
+        {
+            StringBuilder stringBuilder = new StringBuilder(richTextBox.Text);
+            int shift = Decimal.ToInt32(numericUpDownKey.Value);
+            for (int i = 0; i < stringBuilder.Length; i++)
+                if (comboBoxAction.SelectedItem != null)
+                    switch (comboBoxAction.SelectedItem.ToString())
+                    {
+                        case "Расшифровать": stringBuilder[i] = NewCharDecrypt(stringBuilder[i], shift); break;
+                        case "Зашифровать": stringBuilder[i] = NewCharCipher(stringBuilder[i], shift); break;
+                    }
+            richTextBox.Clear();
+            richTextBox.Text = stringBuilder.ToString();
+        }
+
+        private void toolStripMenuItemOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Text Documents|*.txt;*.rtf";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = openFileDialog.OpenFile();
+                richTextBox.LoadFile(stream, RichTextBoxStreamType.PlainText);
+                stream.Close();
+            }
+        }
+
+        private void toolStripMenuItemSave_Click(object sender, EventArgs e)
+        {
+            MemoryStream memorystream = new MemoryStream();
+            Stream filestream;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filestream = saveFileDialog.OpenFile();
+                memorystream.Position = 0;
+                richTextBox.SaveFile(memorystream, RichTextBoxStreamType.PlainText);
+                memorystream.WriteTo(filestream);
+                filestream.Close();
+            }
+        }
+
+        private void toolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripMenuItemBackgroundColor_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            this.BackColor = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+        }
+
+        private void toolStripMenuItemButtonStyle_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            buttonDo.BackColor = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+        }
+    }
+}
